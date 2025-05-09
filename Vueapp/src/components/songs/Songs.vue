c9cb4eae79b23ed3a93bb3aa2db37950d0c1c1f8
tomto <tomasz.zdrojewski@student.htw-berlin.de>
2025-05-09 12:49:28 +0200

    Vue + Component

---
+++ Vueapp/src/components/songs/Songs.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+Hallo
+</template>
+
+<style scoped>
+
+</style>
---
+++ Vueapp/src/router/index.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Songs from "@/components/songs/Songs.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/songs',
+      name: 'songs',
+      component: Songs    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+  ],
+})
+
+export default router
