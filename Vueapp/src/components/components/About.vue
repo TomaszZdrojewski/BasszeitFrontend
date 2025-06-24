19d5e3149106a52537f22fb6a9a7bf39f46f694c
TomaszZdrojewski <Tomasz.Zdrojewski@Student.HTW-Berlin.de>
2025-06-24 19:43:55 +0200

    frontend update

---
+++ Vueapp/src/components/components/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      This page is a part of the Vue 3 + Vite starter template.
+      It is designed to help you get started with your Vue.js project quickly.
+      You can customize this page to include information about your project, team, or anything else you find relevant.
+    </p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
